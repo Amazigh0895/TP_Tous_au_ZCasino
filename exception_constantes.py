0884da78d208eb7c9fc4df1f5106ee27a22cb14a
Amazigh0895 <arab.fechetah@outlook.fr>
2024-04-28 14:05:49 +0200

    rajout du fichier exception_constantes.py

---
+++ exception_constantes.py
@@ -0,0 +1,8 @@
+# Définition des constantes pour les exceptions
+VALUE_ERROR = "VALUE_ERROR: Une erreur de valeur s'est produite."
+TYPE_ERROR = "TYPE_ERROR: Une erreur de type s'est produite."
+ERO_DIVISION_ERROR = "ZERO_DIVISION_ERROR: Une erreur de division par zéro s'est produite."
+INDEX_ERROR = "INDEX_ERROR: Une erreur d'index s'est produite."
+KEY_ERROR = "KEY_ERROR: Une erreur de clé s'est produite."
+FILE_NOT_FOUND_ERROR = "FILE_NOT_FOUND_ERROR: Une erreur de fichier non trouvé s'est produite."
+IO_ERROR = "IO_ERROR: Une erreur d'entrée/sortie s'est produite."
